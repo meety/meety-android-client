7d81cd04c9638c387ae12df4bf9b1ffdec13d577
Tales Tenorio de Souza Pimentel <tales.tsp@gmail.com>
2014-01-20 19:02:05 -0300

    Actions applied to the buttons. Text fields inserted and its get function. A dummy method for setting the app language was created in FirstScreenActivity.java

--- src/com/example/meety_android_client/FirstScreenActivity.java
+++ src/com/example/meety_android_client/FirstScreenActivity.java
@@ -1,9 +1,10 @@
 package com.example.meety_android_client;
 
-import com.example.meety_android_client.util.SystemUiHider;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -13,17 +14,44 @@ import android.os.Bundle;
  */
 public class FirstScreenActivity extends Activity {
 
+	//Future plans: these texts will be set depending on the user language
+	private String userName;
+	private final String passwordDefault = "meety";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_first_screen);
+		
+		setTextByLanguage();
+		final EditText userNameText = (EditText) findViewById(R.id.text_username);
+		final EditText passwordText = (EditText) findViewById(R.id.text_password);
+		userNameText.setText(userName);
+		passwordText.setText(passwordDefault);
+		
+		final Button logInButton = (Button) findViewById(R.id.button_log_in);
+		
+		System.out.println("Log In Button created");
+		logInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Log In Button click");
+                System.out.println("Username: " + userNameText.getText());
+                System.out.println("Password: " + passwordText.getText());
+                System.out.println("========================================");
+            }
+        });
 	}
-
+	
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 
+	}
+	
+	//dummy method to set user language for texts
+	private void setTextByLanguage(){
+		userName = "username";
 	}
 
 }
