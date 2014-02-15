487cc05f3118685331e8c04965151d9b606d4aa7
Tales Tenorio de Souza Pimentel <tales.tsp@gmail.com>
2014-02-15 19:42:38 -0300

    TCC04 Session request screen (without server connection) done. A menu for 'about', 'settings' and 'friendlist' also done.

--- src/com/example/meety_android_client/LoggedInActivity.java
+++ src/com/example/meety_android_client/LoggedInActivity.java
@@ -4,6 +4,10 @@ import com.example.meety_android_client.util.SystemUiHider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -12,15 +16,46 @@ import android.os.Bundle;
  * @see SystemUiHider
  */
 public class LoggedInActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_logged_in);
 
-
 	}
 
+	public void sessionRequest(View view) {
+		// TODO Popup window to inform that the status of the request: 'calling'
+		// or 'missed'
+		System.out.println("Requiring meety session");
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.meety_menu, menu);
+	    return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    System.out.println("item menu >>> " + item.getItemId());
+		// Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.about:
+	            //TODO method call for "about"
+	            return true;
+	        case R.id.settings:
+	        	//TODO method call for "settings"
+	            return true;
+	        case R.id.friendlist:
+	        	//TODO method call for "friendlist"
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 	
 }
